15e8abe28b7b41dc24b4ad7b51e61c1e27ad0d4d
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-07-13 18:44:05 +0800

    ignore target modules & modules_install if CONFIG_MODULES is not set

--- core/kernel.mk
+++ core/kernel.mk
@@ -18,21 +18,26 @@ ifneq ($(TARGET_ARCH),$(HOST_ARCH))
 mk_kernel += CROSS_COMPILE=$(CURDIR)/$(TARGET_TOOLS_PREFIX)
 endif
 
+KERNEL_CONFIG_FILE := kernel/arch/$(TARGET_ARCH)/configs/$(TARGET_KERNEL_CONFIG)
+MOD_ENABLED := $(shell grep ^CONFIG_MODULES=y $(KERNEL_CONFIG_FILE))
+
 # I understand Android build system discourage to use submake,
 # but I don't want to write a complex Android.mk to build kernel.
 # This is the simplest way I can think.
 BUILT_KERNEL_TARGET := $(KBUILD_OUTPUT)/arch/$(TARGET_ARCH)/boot/$(KERNEL_TARGET)
-$(BUILT_KERNEL_TARGET): kernel/arch/$(TARGET_ARCH)/configs/$(TARGET_KERNEL_CONFIG) | $(KBUILD_OUTPUT)
+$(BUILT_KERNEL_TARGET): $(KERNEL_CONFIG_FILE) | $(KBUILD_OUTPUT)
 	$(mk_kernel) $(TARGET_KERNEL_CONFIG)
-	$(mk_kernel) $(KERNEL_TARGET) modules
+	$(mk_kernel) $(KERNEL_TARGET) $(if $(MOD_ENABLED),modules)
 
 $(INSTALLED_KERNEL_TARGET): $(BUILT_KERNEL_TARGET) | $(ACP)
 	$(transform-prebuilt-to-target)
 ifdef TARGET_PREBUILT_MODULES
 	$(ACP) -r $(TARGET_PREBUILT_MODULES) $(TARGET_OUT)/lib
 else
+ifneq ($(MOD_ENABLED),)
 	$(mk_kernel) INSTALL_MOD_PATH=$(CURDIR)/$(TARGET_OUT) modules_install
 	$(hide) rm -f $(TARGET_OUT)/lib/modules/*/{build,source}
+endif
 endif
 
 installclean: FILES += $(KBUILD_OUTPUT) $(INSTALLED_KERNEL_TARGET)
