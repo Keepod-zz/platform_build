65f99e9cbb2511e701e15af94e06bd61ca9d3913
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-12-10 16:16:54 +0800

    add TARGET_PREBUILT_APPS

--- target/board/generic_x86/AndroidBoard.mk
+++ target/board/generic_x86/AndroidBoard.mk
@@ -1,3 +1,15 @@
+#
+# Copyright (C) 2009 The Android-x86 Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+
+$(call add-prebuilt-targets,$(TARGET_OUT),$(TARGET_PREBUILT_APPS))
+
 LOCAL_PATH := $(call my-dir)
 DEFAULT_WPA_SUPPLICANT_CONF_DIR := $(LOCAL_PATH)
 
