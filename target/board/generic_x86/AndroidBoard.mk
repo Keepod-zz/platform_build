8aa0dc1b61e665c614d1350bedd8adc1936f49f9
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-12-10 15:50:00 +0800

    add wifi support for x86
    If an inherited target needs to support wifi, it has to define
    BOARD_WPA_SUPPLICANT_DRIVER and BOARD_WPA_SUPPLICANT_CONF_DIR(optional).

--- target/board/generic_x86/AndroidBoard.mk
+++ target/board/generic_x86/AndroidBoard.mk
@@ -1,4 +1,27 @@
 LOCAL_PATH := $(call my-dir)
+DEFAULT_WPA_SUPPLICANT_CONF_DIR := $(LOCAL_PATH)
+
 $(call add-prebuilt-target,$(TARGET_ROOT_OUT),init.rc)
 
 include $(call all-subdir-makefiles)
+
+define include-wpa-supplicant-conf
+LOCAL_PATH := $(1)
+include $$(CLEAR_VARS)
+LOCAL_MODULE := wpa_supplicant.conf
+LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $$(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $$(LOCAL_MODULE)
+LOCAL_PREBUILT_STRIP_COMMENTS := 1
+include $$(BUILD_PREBUILT)
+endef
+
+define add-wpa-supplicant-conf
+$(eval $(include-wpa-supplicant-conf))
+endef
+
+ifneq ($(BOARD_WPA_SUPPLICANT_DRIVER),)
+BOARD_WPA_SUPPLICANT_CONF_DIR ?= $(DEFAULT_WPA_SUPPLICANT_CONF_DIR)
+$(call add-wpa-supplicant-conf,$(BOARD_WPA_SUPPLICANT_CONF_DIR))
+endif
