2d6cb0396ef2a0f6edc5169e63a3ef17f5deb623
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-03-05 21:07:25 +0800

    definitions.mk: fix a bug in build-package-rules
    We cannot call all-subdir-makefiles directly since
    it has included other files.

--- core/definitions.mk
+++ core/definitions.mk
@@ -413,7 +413,7 @@ LOCAL_SRC_FILES := $$(call all-java-files-under,src)
 LOCAL_PACKAGE_NAME := $$(notdir $$(LOCAL_PATH))
 LOCAL_CERTIFICATE := $(2)
 include $$(BUILD_PACKAGE)
-include $$(call all-subdir-makefiles)
+include $$(call all-makefiles-under,$$(LOCAL_PATH))
 
 endef
 
